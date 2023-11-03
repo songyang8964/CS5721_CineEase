684c4bdb55e3c0e32970d6bc660eceec7002cb15
moro77747 <447351372@qq.com>
2023-11-03 14:33:26 +0000

    create project

---
+++ src/main/java/com/ywxx/cineEase/CineEaseApplication.java
@@ -0,0 +1,13 @@
+package com.ywxx.cineEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ywxx/cineEase/CineEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywxx.cineEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
